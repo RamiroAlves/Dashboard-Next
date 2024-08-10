69762f6663bd0b3f7ef6dade25c3bc63ca6117db
ramiro <ramiro@fortalshop.com>
2024-08-10 22:00:22 +0000

    Criado projeto com Next.js e Tailwind CSS.

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main
+      className={cn(
+        "min-h-screen bg-background font-sans antialiased",
+        inter.className
+      )}
+    >
+      <p>Hello World!</p>
+    </main>
+  );
+}
