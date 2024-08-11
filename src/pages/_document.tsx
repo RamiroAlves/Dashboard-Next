63f87cc045d1558c6a7eab18be9bc6401c9336ce
ramiro <ramiro@fortalshop.com>
2024-08-11 13:40:06 +0000

    Adicionado components sidebar

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { Sidebar } from "@/components/Sidebar";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <Sidebar />
         <Main />
         <NextScript />
       </body>
